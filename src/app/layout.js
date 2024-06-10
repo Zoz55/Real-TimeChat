60b40923cd26153d72a8e59f0c12b2a0dade85bd
Fayez Kassem <101155960+Zoz55@users.noreply.github.com>
2024-06-10 06:55:21 +0300

    added fonts

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,6 @@
-import { Inter } from "next/font/google";
+import { inter } from "./fonts";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Create Next App",
@@ -11,6 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      {/* <body >{children}</body> */}
       <body className={inter.className}>{children}</body>
     </html>
   );
