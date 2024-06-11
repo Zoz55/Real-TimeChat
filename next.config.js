f9fae51e2dd73c7b0b8d5a142b1a78048f2d98c2
Fayez Kassem <101155960+Zoz55@users.noreply.github.com>
2024-06-11 22:14:30 +0300

    changed the manifest location

--- next.config.js
+++ next.config.js
@@ -1,13 +1,7 @@
 const withPWA = require('next-pwa')({
   dest: 'public',
-  register: true,
-  skipWaiting: true,
 });
 
 module.exports = withPWA({
-  reactStrictMode: true,
-  experimental: {
-    appDir: true,
-  },
   // Other Next.js config options
 });
